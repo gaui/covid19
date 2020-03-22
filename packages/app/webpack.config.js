9c759b130ad074de375154fb044b5eee6432aa47
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-22 02:05:24 +0000

    Fixing package module declarations

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/App.tsx',
   output: {
-    filename: 'app.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
