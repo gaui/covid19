d8492f133c09f27fe6498823c4571055602e55ef
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-22 18:09:54 +0000

    Replaced html-loader-jest with jest-raw-loader

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.((j|t)sx?|svg|css)$': 'babel-jest',
-    '^.+\\.html?$': 'html-loader-jest'
+    '^.+\\.((j|t)sx?|css)$': 'babel-jest',
+    '^.+\\.(html|svg)?$': 'jest-raw-loader'
   },
   coverageDirectory: '.coverage',
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__mocks__/']
