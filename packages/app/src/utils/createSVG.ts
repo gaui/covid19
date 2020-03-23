37255fe0e8b4405f77c44df444aba4cc9bf5cb33
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-23 16:26:26 +0000

    Fixed bug when SVG element is not defined

--- packages/app/src/utils/createSVG.ts
+++ packages/app/src/utils/createSVG.ts
@@ -3,7 +3,9 @@ import React from 'react';
 export const createSVG = (svg: string) => {
   const svgContainer = document.createElement('div');
   svgContainer.innerHTML = svg;
-  const svgElement = svgContainer.firstElementChild as Element;
+  const svgElement = (svgContainer.firstElementChild as Element) || {
+    attributes: {}
+  };
   const props = Object.fromEntries(
     Array.from(svgElement.attributes).map(x => [x.name, x.value])
   );
