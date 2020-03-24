b7b8ceb63e7c136a4311706f9638b56da27918a3
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-24 18:15:26 +0000

    Wrap the top-level App component in with the Redux React Provider

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,10 +1,12 @@
 import './styles.css';
 import { provider } from '@gaui/covid19-core';
-import React, { Suspense } from 'react';
 import { LazyComponent } from './components/LazyComponent';
+import React, { Suspense } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export const App = () => (
-  <>
+  <Provider store={store}>
     <header>COVID-19</header>
     <LazyComponent
       component="StatsCardContainer"
@@ -12,5 +14,5 @@ export const App = () => (
       provider={provider}
     />
     <footer>© gaui.is</footer>
-  </>
+  </Provider>
 );
