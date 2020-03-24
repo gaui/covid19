24dd257d12ef3da33cd5ac0ed2a806410c411ae2
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-24 22:22:29 +0000

    Created a LazyComponent that handles dynamic importing components

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,12 +1,16 @@
 import './styles.css';
 import { provider } from '@gaui/covid19-core';
-import StatsCardContainer from './components/StatsCardContainer';
-import React from 'react';
+import React, { Suspense } from 'react';
+import { LazyComponent } from './components/LazyComponent';
 
 export const App = () => (
   <>
     <header>COVID-19</header>
-    <StatsCardContainer interval={0} provider={provider} />
+    <LazyComponent
+      component="StatsCardContainer"
+      interval={0}
+      provider={provider}
+    />
     <footer>© gaui.is</footer>
   </>
 );
