5bc4369fdfb0cd921dcf1e1cf6ece504ed5b0273
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-28 01:25:50 +0000

    Changed width/height for Chrome extension

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 if (window.chrome && chrome.runtime && chrome.runtime.id) {
-  document.documentElement.style.setProperty('--width', '300px');
-  document.documentElement.style.setProperty('--height', '500px');
+  document.documentElement.style.setProperty('--width', '450px');
+  document.documentElement.style.setProperty('--height', '430px');
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
