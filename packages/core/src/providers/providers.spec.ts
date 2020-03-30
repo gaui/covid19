9bc77bbb9dad493746e71854e2ad5c29d42bf71d
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-30 23:52:42 +0000

    [core] Added a test for merge behavior from both providers

---
+++ packages/core/src/providers/providers.spec.ts
@@ -0,0 +1,54 @@
+import { getData } from '.';
+
+jest.mock('./covid-is', () => ({
+  getData: jest.fn(() =>
+    Promise.resolve({
+      hospitalized: 30,
+      quarantineIn: 9236,
+      quarantinePost: 5427,
+      samples: 16484,
+      recovered: 157,
+      critical: 10,
+      isolated: 927,
+      infected: 1086
+    })
+  )
+}));
+
+jest.mock('./covid-ext', () => ({
+  getData: jest.fn(() =>
+    Promise.resolve({
+      cases: 1086,
+      todayCases: 66,
+      deaths: 2,
+      todayDeaths: 0,
+      recovered: 157,
+      active: 927,
+      critical: 25
+    })
+  )
+}));
+
+describe('Providers', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should get merged data from all providers', async () => {
+    const data = await getData();
+    expect(data).toEqual({
+      hospitalized: 30,
+      todayCases: 66,
+      deaths: 2,
+      todayDeaths: 0,
+      quarantineIn: 9236,
+      quarantinePost: 5427,
+      samples: 16484,
+      recovered: 157,
+      critical: 10,
+      isolated: 927,
+      active: 927,
+      cases: 1086
+    });
+  });
+});
