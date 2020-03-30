379eec052b2b9744b5baa71c283238faaf419cde
Gudjon Jonsson <gudjonj@gmail.com>
2020-03-30 23:52:02 +0000

    [core] Added Icelandic provider

--- packages/core/src/providers/index.ts
+++ packages/core/src/providers/index.ts
@@ -1,7 +1,8 @@
+import { getData as covidIs } from './covid-is';
 import { getData as covidExt } from './covid-ext';
 import * as R from 'ramda';
 
-const providers = [covidExt];
+const providers = [covidIs, covidExt];
 
 const getData = async () => {
   const providerData = await Promise.all(providers.map(p => p()));
