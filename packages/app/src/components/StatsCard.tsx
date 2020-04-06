f853b0a8e7c1fd3f66ce49c8c9ce9ba152176280
Gudjon Jonsson <gudjonj@gmail.com>
2020-04-06 04:58:25 +0000

    [app] Use new thousand seperator utility in StatsCard component

--- packages/app/src/components/StatsCard.tsx
+++ packages/app/src/components/StatsCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
 import { StatsCardProps } from '../types/components';
+import { thousandSeperator } from '../utils/formatting';
 
 const StatsCard = ({ icon, count, title, ...props }: StatsCardProps) => {
   const Icon = () => icon;
@@ -9,7 +10,9 @@ const StatsCard = ({ icon, count, title, ...props }: StatsCardProps) => {
     <div {...props}>
       <Icon />
       <div className="title">{title}</div>
-      <div className="stats">{count == null ? '-' : count}</div>
+      <div className="stats">
+        {count == null ? '-' : thousandSeperator(count)}
+      </div>
     </div>
   );
 };
