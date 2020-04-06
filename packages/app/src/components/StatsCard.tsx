1c406a7c1d5c2d7eb2d89b16ae71b2101836c237
Gudjon Jonsson <gudjonj@gmail.com>
2020-04-06 03:25:16 +0000

    [app] Added the title above each card + updated snapshots

--- packages/app/src/components/StatsCard.tsx
+++ packages/app/src/components/StatsCard.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { StatsCardProps } from '../types/components';
 
-const StatsCard = ({ icon, count, ...props }: StatsCardProps) => {
+const StatsCard = ({ icon, count, title, ...props }: StatsCardProps) => {
   const Icon = () => icon;
 
   return (
     <div {...props}>
       <Icon />
-      <div>{count == null ? '-' : count}</div>
+      <div className="title">{title}</div>
+      <div className="stats">{count == null ? '-' : count}</div>
     </div>
   );
 };
