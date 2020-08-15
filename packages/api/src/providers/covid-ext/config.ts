22131065c8203dbe02b357b446dd297082d77bf4
Gudjon Jonsson <gudjonj@gmail.com>
2020-08-15 17:13:34 +0000

    [api] refactor: Changed URL of COVID-19 data source to disease.sh v3
    Closes #86

--- packages/api/src/providers/covid-ext/config.ts
+++ packages/api/src/providers/covid-ext/config.ts
@@ -1,7 +1,7 @@
 import { Covid19ProviderConfig } from '../../schema';
 
 const config: Covid19ProviderConfig = {
-  url: 'https://corona.lmao.ninja/v2/countries/iceland',
+  url: 'https://disease.sh/v3/covid-19/countries/iceland',
 };
 
 export default config;
