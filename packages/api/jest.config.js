d32b16a226bd31bc600ec4e82bebb0e156870a95
Gudjon Jonsson <gudjonj@gmail.com>
2020-08-15 17:17:14 +0000

    [api] chore: Added a Jest config file to run tests

---
+++ packages/api/jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jestBaseConfig = require('../../jest.base.config');
+
+module.exports = {
+  ...jestBaseConfig,
+  displayName: 'api',
+  testEnvironment: 'node',
+};
